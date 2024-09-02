6be180c33cedb84aeb07696c92ef25ae064e39f5
pushpakdeore <deorepushpakk@gmail.com>
2024-09-02 11:22:12 +0530

    added: Play the game with two player

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,34 +1,55 @@
 import java.util.*;
 public class SnakeLadder {
     public static void main(String[] args) {
-        int position = 0;
-        int rollCount = 0;
+        int position1 = 0, position2 = 0;
+        int rollCount1 = 0, rollCount2 = 0;
+        boolean player1Turn = true;
 
-        System.out.println("Starting the Snake and Ladder game...");
+        System.out.println("Starting the Snake and Ladder game with 2 players...");
 
-        while (position < 100) {
-            int dieRoll = rollDie();
-            rollCount++;
-            System.out.println("Roll " + rollCount + ": Rolled a " + dieRoll);
-
-            int option = checkOption();
-            if (option == 0) {
-                System.out.println("No Play. Stay at position " + position);
-            } else if (option == 1) {
-                if (position + dieRoll <= 100) {
-                    position += dieRoll;
-                }
-                System.out.println("Ladder! Move to position " + position);
+        while (position1 < 100 && position2 < 100) {
+            if (player1Turn) {
+                rollCount1++;
+                position1 = playTurn(position1, rollCount1, "Player 1");
+                if (checkWin(position1, "Player 1")) break;
+                player1Turn = false;
             } else {
-                position -= dieRoll;
-                if (position < 0) {
-                    position = 0;
-                }
-                System.out.println("Snake! Move back to position " + position);
+                rollCount2++;
+                position2 = playTurn(position2, rollCount2, "Player 2");
+                if (checkWin(position2, "Player 2")) break;
+                player1Turn = true;
             }
         }
+    }
 
-        System.out.println("Congratulations! You've reached the winning position 100 in " + rollCount + " rolls!");
+    public static int playTurn(int position, int rollCount, String player) {
+        int dieRoll = rollDie();
+        System.out.println(player + " Roll " + rollCount + ": Rolled a " + dieRoll);
+
+        int option = checkOption();
+        if (option == 0) {
+            System.out.println(player + ": No Play. Stay at position " + position);
+        } else if (option == 1) {
+            if (position + dieRoll <= 100) {
+                position += dieRoll;
+            }
+            System.out.println(player + ": Ladder! Move to position " + position);
+        } else {
+            position -= dieRoll;
+            if (position < 0) {
+                position = 0;
+            }
+            System.out.println(player + ": Snake! Move back to position " + position);
+        }
+        return position;
+    }
+
+    public static boolean checkWin(int position, String player) {
+        if (position == 100) {
+            System.out.println("Congratulations! " + player + " has won the game!");
+            return true;
+        }
+        return false;
     }
 
     public static int rollDie() {
