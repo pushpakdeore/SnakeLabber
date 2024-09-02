64f0bdd55cc4e9d11416d27db64578f4df36998a
pushpakdeore <deorepushpakk@gmail.com>
2024-09-02 11:14:14 +0530

    Added the Player gets Exact Winning Position

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,37 +1,43 @@
 import java.util.*;
 public class SnakeLadder {
     public static void main(String[] args) {
-        int position = 0; // Start position
+        int position = 0;
+
         System.out.println("Starting the Snake and Ladder game...");
 
-        // Roll the die
-        int dieRoll = rollDie();
-        System.out.println("Rolled a " + dieRoll);
+        while (position < 100) {
+            int dieRoll = rollDie();
+            System.out.println("Rolled a " + dieRoll);
 
-        // Check the option
-        int option = checkOption();
-        if (option == 0) {
-            System.out.println("No Play. Stay at position " + position);
-        } else if (option == 1) {
-            position += dieRoll;
-            System.out.println("Ladder! Move to position " + position);
-        } else {
-            position -= dieRoll;
-            if (position < 0) {
-                position = 0;
+            int option = checkOption();
+            if (option == 0) {
+                System.out.println("No Play. Stay at position " + position);
+            } else if (option == 1) {
+                if (position + dieRoll <= 100) {
+                    position += dieRoll;
+                }
+                System.out.println("Ladder! Move to position " + position);
+            } else {
+                position -= dieRoll;
+                if (position < 0) {
+                    position = 0;
+                }
+                System.out.println("Snake! Move back to position " + position);
             }
-            System.out.println("Snake! Move back to position " + position);
         }
 
+        System.out.println("Congratulations! You've reached the winning position 100!");
+
     }
+
     public static int rollDie() {
         Random random = new Random();
-        return random.nextInt(6) + 1; // Generates a random number between 1 and 6
+        return random.nextInt(6) + 1;
     }
 
     public static int checkOption() {
         Random random = new Random();
-        return random.nextInt(3); // Generates a random number between 0, 1, 2 (No Play, Ladder, Snake)
+        return random.nextInt(3);
     }
 
 }
